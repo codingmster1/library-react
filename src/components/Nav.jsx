30ec18269797035caf8ae6dc219b61085fcc90ee
Jeff <doylejeff98@gmail.com>
2023-08-09 13:28:55 -0400

    menu list and new file landing components

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ const Nav = () => {
     return (
     
     <nav>
-        <div class="nav__container">
+        <div className="nav__container">
             <a href="/">
                 <img src={LibraryLogo} alt="" className="logo"/>
             </a>
@@ -32,6 +32,28 @@ const Nav = () => {
                     <span className ="cart__length">2</span>
                 </li>
             </ul>
+            <div className = "menu__backdrop">
+                <button className="btn__menu btn__menu--close">
+                    <FontAwesomeIcon icon="times" />
+                    </button> 
+                    <ul className="menu__links">
+                        <li className="menu__list">
+                            <a href="/" className="menu__link">
+                                Home
+                            </a>
+                        </li>
+                        <li className="menu__list">
+                            <a href="/books" className="menu__link">
+                                Books
+                            </a>
+                        </li>
+                        <li className="menu__list">
+                            <a href="/cart" className="menu__link">
+                                Cart
+                            </a>
+                        </li>
+                    </ul>
+            </div>
         </div>
 
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {faBars, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import {faBars, faShoppingCart, faTimes} from '@fortawesome/free-solid-svg-icons';
 
-library.add(faBars, faShoppingCart)
+library.add(faBars, faShoppingCart, faTimes)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
